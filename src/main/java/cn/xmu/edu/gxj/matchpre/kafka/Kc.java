3caaef20f4dbb166676d10bbaa5502ce9e857b20
cdmaok <cdmaok@126.com>
2016-06-22 00:26:16 +0800

    figure consumer

--- src/main/java/cn/xmu/edu/gxj/matchpre/kafka/Kc.java
+++ src/main/java/cn/xmu/edu/gxj/matchpre/kafka/Kc.java
@@ -1,5 +1,6 @@
 package cn.xmu.edu.gxj.matchpre.kafka;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -10,11 +11,14 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
 
 public class Kc {
 
+	public static String ip = "121.192.180.198:9092";
+
 	public static void produce(){ Properties props = new Properties();
-	 props.put("bootstrap.servers", "121.192.180.198:9092");
+	 props.put("bootstrap.servers", ip);
 	 props.put("acks", "all");
 	 props.put("retries", 0);
 	 props.put("batch.size", 16384);
@@ -25,7 +29,8 @@ public class Kc {
 
 	 Producer<String, String> producer = new KafkaProducer<>(props);
 	 for(int i = 0; i < 100; i++){
-	     producer.send(new ProducerRecord<String, String>("matchp", Integer.toString(i), Integer.toString(i)));
+//	     producer.send(new ProducerRecord<String, String>("matchp", Integer.toString(i), Integer.toString(i)));
+	     producer.send(new ProducerRecord<String, String>("matchp", null, "{\"foo\":\"bar\"}"));
 	     producer.flush();
 	 }
 
@@ -39,7 +44,7 @@ public class Kc {
 	
 	public static void consume(){
 	     Properties props = new Properties();
-	     props.put("bootstrap.servers", "121.19.180.198:9092");
+	     props.put("bootstrap.servers", ip);
 	     props.put("group.id", "test");
 	     props.put("enable.auto.commit", "true");
 	     props.put("auto.commit.interval.ms", "1000");
@@ -47,13 +52,11 @@ public class Kc {
 	     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-	     consumer.subscribe(Arrays.asList("matchp", "lofter"));
-	     while (true) {
-	         ConsumerRecords<String, String> records = consumer.poll(100);
-	         System.out.println(records.count());
-	         for (ConsumerRecord<String, String> record : records)
-	             System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
-	     }
+	     consumer.subscribe(Arrays.asList("matchp"));
+         ConsumerRecords<String, String> records = consumer.poll(10000);
+         System.out.println(records.count());
+         for (ConsumerRecord<String, String> record : records)
+             System.out.printf("offset = %d, key = %s, value = %s,", record.offset(), record.key(), record.value());
 	}
 
 }
